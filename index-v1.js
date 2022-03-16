cd236a8507ed496c8496bc738290ab4680d7342e
Paulo Coneglian <paulo.coneglian@foreverliss.com.br>
2022-03-16 10:44:56 -0300

    Atualização rateio kits

---
+++ index-v1.js
@@ -0,0 +1,20 @@
+const util = require('util');
+
+(async () => {
+  const db = require('./db');
+    const orders = await db.selectOrders();                
+    
+    for (const [index, order] of orders.entries()) {
+      const hostname = order.hostname;
+      const sequencePK = order.sequence;
+      const orderItems = await db.selectOrderItems(hostname, sequencePK);
+      const payloadResponse = await db.payloadSend(order, orderItems);      
+
+      // console.log('\n************************************************');
+      // console.log(`------------------| PEDIDO ${index+1} |------------------`);
+      // console.log('************************************************\n');
+      // console.log(util.inspect(payloadResponse, false, null, true));
+    }
+})();
+
+/** email Eduarda Couto: duda_rasmus@hotmail.com */
--- index.js
+++ index.js
@@ -2,23 +2,16 @@ const util = require('util');
 
 (async () => {
   const db = require('./db');
-    const orders = await db.selectOrders();                
-    
-    for (const [index, order] of orders.entries()) {
-      const sequencePK = order.sequence;
-      const orderItems = await db.selectOrderItems(order, sequencePK);
-
-      //console.log(orderItems);
-      // const payloadResponse = await db.payloadSend(order, orderItems);
+    // const teste = await db.teste();
+    // console.log(teste);
+    const orders = await db.selectOrders();
+    for (const [index, order] of orders.entries()) {            
       
-
-      //console.log(payloadResponse);
-
-      // console.log('\n************************************************');
-      // console.log(`------------------| PEDIDO ${index+1} |------------------`);
-      // console.log('************************************************\n');
-      // console.log(util.inspect(payloadResponse, false, null, true));
+      const orderItemsKits = await db.selectOrderKitItems(order);      
+      
+      console.log('\n************************************************');
+      console.log(`------------------| PEDIDO ${index+1} |------------------`);
+      console.log('************************************************\n');
+      console.log(util.inspect(orderItemsKits, false, null, true));
     }
 })();
-
-/** email Eduarda Couto: duda_rasmus@hotmail.com */
